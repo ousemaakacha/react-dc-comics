5534c2f816eb78f72ead08793c50a3fca5c6353e
ousemaakacha <akachaousema@gmail.com>
2025-11-04 17:52:36 +0100

    links section completition

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Header from "./components/Header";
 // import DCLOGO from "./img/dc-logo.png";
+import COMDIG from "./img/buy-comics-digital-comics.png";
+import MERCH from "./img/buy-comics-merchandise.png";
+import SHOP from "./img/buy-comics-shop-locator.png";
+import SUBS from "./img/buy-comics-subscriptions.png";
+import VISA from "./img/buy-dc-power-visa.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +23,32 @@ function App() {
             <p> -- CONTENT GOES HERE --</p>
           </div>
         </section>
-        <section></section>
+        <section class="colLinks">
+          <div class="container text-center">
+            <div class="row row-cols-5 ">
+              <div class="col d-flex align-items-center gap-3 ">
+                <img height={60} src={COMDIG} />
+                <p>DIGITAL COMICS</p>
+              </div>
+              <div class="col d-flex align-items-center gap-3">
+                <img src={MERCH} height={60} />
+                <p>DC MERCHANDISE</p>
+              </div>
+              <div class="col d-flex align-items-center gap-3">
+                <img src={SHOP} height={60} />
+                <p>COMIC SHOP LOCATOR</p>
+              </div>
+              <div class="col d-flex align-items-center gap-3">
+                <img src={SUBS} height={60} />
+                <p>SUBSCRIPTION</p>
+              </div>
+              <div class="col d-flex align-items-center gap-3">
+                <img src={VISA} height={50} />
+                <p>DC POWER VISA</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
