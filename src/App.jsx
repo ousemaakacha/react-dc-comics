62e247d5aae6da23f61d81854f3bcce574ae4a9a
ousemaakacha <akachaousema@gmail.com>
2025-11-04 14:16:23 +0100

    FIRST COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
