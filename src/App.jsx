10d7d53d4fcd965fef8c82ed226cd0061ca74026
ousemaakacha <akachaousema@gmail.com>
2025-11-04 15:43:21 +0100

    header completition

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Header from "./components/Header";
+// import DCLOGO from "./img/dc-logo.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <header>
-        <div>
-          <ul class="nav nav-underline justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                CHARACTERS
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                COMICS
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                MOVIES
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link">TV</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                GAMES
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                COLLECTIBLES
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                VIDEOS
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link">FANS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                NEWS
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                SHOP
-              </a>
-            </li>
-          </ul>
-        </div>
-      </header>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,64 @@
+import DCLOGO from "../img/dc-logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <nav class="navbar link-offset-10 navbar-expand-lg ">
+        <div class="container">
+          <img src={DCLOGO} height="90" />
+
+          <div>
+            <ul class="navbar-nav d-flex gap-3 ms-auto mb-2 mb-lg-0">
+              <li class="nav-item ">
+                <a class="nav-link " href="#">
+                  CHARACTERS
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  COMICS
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  MOVIES
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">TV</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  GAMES
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  COLLECTIBLES
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  VIDEOS
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">FANS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  NEWS
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  SHOP
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
